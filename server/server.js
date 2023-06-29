043eb05356629cc510fdbc885680079ccede2e9e
Sergio <spinfinite07@gmail.com>
2023-06-29 12:51:15 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// Serve HTML
+
+const express = require('express')
+const cors = require('cors')  
+const path = require('path')
+
+const app = express()
+
+app.use(express.json)
+app.use(cors())
+app.use(express.static('public'))
+
+// Base End Point
+app.get('/', (req, res) => {
+
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+
+})
+
+
+
+app.listen(4000, console.log(`App running on 4000`))
